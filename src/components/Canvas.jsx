cfdacc091716cb7200ecb0bee118dbe213bc59e6
Chupriarti <wallx13@gmail.com>
2022-01-19 22:01:14 +0300

    add Canvas observable

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,12 +1,21 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
+import canvasState from '../store/canvasState';
 import '../styles/canvas.scss';
 
-const Canvas = () => {
+const Canvas = observer( () => {
+  const canvasRef = React.useRef();
+
+  React.useEffect(() => {
+    console.log(canvasRef);
+    canvasState.setCanvas(canvasRef.current);
+  }, []);
+
   return (
     <div className='canvas'>
-      <canvas width={400} height={400}></canvas>
+      <canvas ref={canvasRef} width={400} height={400}></canvas>
     </div>
   )
-}
+});
 
 export default Canvas;
