33b568a60e1f7fccc4c694eca881f3c24c6903dc
Chupriarti <wallx13@gmail.com>
2022-01-20 23:23:20 +0300

    fix

--- src/tools/Eraser.js
+++ src/tools/Eraser.js
@@ -7,8 +7,8 @@ export default class Eraser extends Brush {
     }
 
     draw(x, y) {
-        this.ctx.strokeStyle = "white"
-        this.ctx.lineTo(x, y)
-        this.ctx.stroke()
+        this.ctx.strokeStyle = "white";
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
     }
 }
