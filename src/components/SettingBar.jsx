26c62d0596934f4a4b443d8854063a51a930b2fa
Chupriarti <wallx13@gmail.com>
2022-01-21 22:25:43 +0300

    add line settings control

--- src/components/SettingBar.jsx
+++ src/components/SettingBar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import toolState from '../store/toolState';
 import '../styles/toolbar.scss';
 
 const SettingBar = () => {
   return (
     <div className='setting-bar'>
-      SettingBar
+      <label htmlFor="line-width">Line width</label>
+      <input
+        onChange={e => toolState.setLineWidth(e.target.value)} 
+        style={{margin: '0 10px'}} 
+        id="line-width" 
+        type="number" 
+        defaultValue={1} 
+        min={1} 
+        max={50} />
     </div>
   )
 }
