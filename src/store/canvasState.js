2d18e6569b6e653907ee7c8d7618050342f7686b
Chupriarti <wallx13@gmail.com>
2022-01-22 19:48:52 +0300

    add undo action logic

--- src/store/canvasState.js
+++ src/store/canvasState.js
@@ -20,6 +20,22 @@ class CanvasState {
     pushToRedo (data) {
         this.redoList.push(data);
     }
+
+    undo () {
+        let ctx = this.canvas.getContext('2d');
+        if (this.undoList.length > 0) {
+            let dataUrl = this.undoList.pop();
+            let img = new Image();
+            img.src = dataUrl;
+            img.onload = () => {
+                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+            }
+        } else {
+            ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+    }
+    
 }
 
 export default new CanvasState();
