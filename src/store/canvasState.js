2fa8842dac81df10029449b0adb88e37ab4894ee
Chupriarti <wallx13@gmail.com>
2022-01-22 19:58:13 +0300

    add redo action logic

--- src/store/canvasState.js
+++ src/store/canvasState.js
@@ -25,6 +25,7 @@ class CanvasState {
         let ctx = this.canvas.getContext('2d');
         if (this.undoList.length > 0) {
             let dataUrl = this.undoList.pop();
+            this.redoList.push(this.canvas.toDataURL());
             let img = new Image();
             img.src = dataUrl;
             img.onload = () => {
@@ -36,6 +37,21 @@ class CanvasState {
         }
     }
     
+    redo () {
+        let ctx = this.canvas.getContext('2d');
+        if (this.redoList.length > 0) {
+            let dataUrl = this.redoList.pop();
+            this.undoList.push(this.canvas.toDataURL());
+            let img = new Image();
+            img.src = dataUrl;
+            img.onload = () => {
+                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+            }
+        } else {
+            ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+    }
 }
 
 export default new CanvasState();
