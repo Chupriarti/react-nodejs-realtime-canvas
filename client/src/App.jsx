763cd98e92a69e8d47aef6ec0d0bff46e4f36240
Chupriarti <wallx13@gmail.com>
2022-01-24 23:42:23 +0300

    add unique id user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,23 @@ import Canvas from './components/Canvas';
 import Toolbar from './components/Toolbar';
 import SettingBar from './components/SettingBar';
 import './styles/app.scss';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app'>
-      <Toolbar />
-      <SettingBar />
-      <Canvas />
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Switch>
+          <Route path='/:id'>
+            <Toolbar />
+            <SettingBar />
+            <Canvas />
+          </Route>
+          <Redirect to={`f${(+new Date).toString(16)}`} />
+        </Switch>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
