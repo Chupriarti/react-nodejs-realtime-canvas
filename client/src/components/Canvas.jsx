c184996570feb70100fe31fa00a8ce6347e3a932
Chupriarti <wallx13@gmail.com>
2022-01-25 23:50:39 +0300

    prepare Modal input

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import canvasState from '../store/canvasState';
 import toolState from '../store/toolState';
 import '../styles/canvas.scss';
 import Brush from '../tools/Brush';
+import {Modal, Button} from 'react-bootstrap'
 
 const Canvas = observer( () => {
   const canvasRef = React.useRef();
@@ -19,6 +20,17 @@ const Canvas = observer( () => {
 
   return (
     <div className='canvas'>
+      <Modal show={true} onHide={() => {}}>
+        <Modal.Header>
+          <Modal.Title>Enter your name</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Some text here</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() =>{}}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <canvas
         onMouseDown ={() => mouseDownHandler()}
         ref={canvasRef} 
