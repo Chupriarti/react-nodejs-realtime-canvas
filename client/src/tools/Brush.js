f0bd81e4bc36127c3da89b4c0a8b2f07d319946b
Chupriarti <wallx13@gmail.com>
2022-01-27 22:40:24 +0300

    edit brush to use with socket

--- client/src/tools/Brush.js
+++ client/src/tools/Brush.js
@@ -24,12 +24,21 @@ export default class Brush extends Tool {
 
     mouseMoveHandler(e) {
         if (this.mouseDown) {
-            this.draw(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
+            //this.draw(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
+            this.socket.send(JSON.stringify({
+                method: "draw",
+                id: this.id,
+                figure: {
+                    type: "brush",
+                    x: e.pageX - e.target.offsetLeft,
+                    y: e.pageY - e.target.offsetTop
+                }
+            }))
         }
     }
 
-    draw(x, y) {
-        this.ctx.lineTo(x, y);
-        this.ctx.stroke();
+    static draw(ctx, x, y) {
+        ctx.lineTo(x, y);
+        ctx.stroke();
     }
 }
